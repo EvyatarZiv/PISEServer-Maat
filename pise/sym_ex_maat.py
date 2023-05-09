0e9c5f42a4f8d596ecba7d98fd60f556b780f5a3
EvyatarZiv <evyatarziv@gmail.com>
2023-05-09 20:28:55 +0300

    Changed main loop logic

--- pise/sym_ex_maat.py
+++ pise/sym_ex_maat.py
@@ -13,19 +13,35 @@ LIB64_PATH = ""  # TODO: Add path to lib64
 class QueryRunner:
     def __init__(self, file, callsites_to_monitor):
         self.file = file
-        self.engine = maat.MaatEngine(maat.ARCH.X64, maat.OS.LINUX)
+        self.engine = sym_ex_helpers_maat.PISEEngine(None, maat.ARCH.X64, maat.OS.LINUX)
         self.mode = None
         self.callsites_to_monitor = callsites_to_monitor
         self.set_membership_hooks()
-        sym_ex_helpers_maat.state_manager = maat.SimpleStateManager("./tmp")
 
-    def set_membership_hooks(self):
+    def set_membership_hooks(self) -> None:
         if self.mode == 'membership':
             return
         logger.info('Setting hooks')
         for callsite in self.callsites_to_monitor:
             callsite.set_hook(self.engine)
         self.mode = 'membership'
+
+    def do_monitoring(self) -> bool:
+        while True:
+            stop_res = self.engine.run()
+            if stop_res == maat.STOP.EXIT:
+                if not self.engine.pop_engine_state():
+                    return False  # Membership is false
+                continue
+            elif stop_res == maat.STOP.HOOK:
+                if self.engine.idx == len(self.engine.inputs):
+                    return True  # Membership is true
+                else:
+                    if not self.engine.pop_engine_state():
+                        return False  # Membership is false
+                    continue
+            else:
+                raise NotImplementedError
 
     def membership_step_by_step(self, inputs: list):
         """
@@ -42,8 +58,8 @@ class QueryRunner:
             logger.info('No prefix exists in cache, starting from the beginning')
             # TODO: Add support for args
             self.engine.load(binary=self.file, type=maat.BIN.ELF64, args=[], libdirs=[LIB64_PATH], load_interp=True)
-            sym_ex_helpers_maat.input_types = inputs
-            sym_ex_helpers_maat.idx = 0
-        while self.engine.run() == maat.STOP.EXIT:
-            if not sym_ex_helpers_maat.pop_engine_state(self.engine):
-                return False  # Membership is false
+            self.engine.inputs = inputs
+        if not self.do_monitoring():
+            return False, None, 0, 0, 0  # Membership is false
+
+
