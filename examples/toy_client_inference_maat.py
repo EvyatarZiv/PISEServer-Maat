6ff5753f5a68c540d70ee1b4e3f2ca07ac00db46
EvyatarZiv <evyatarziv@gmail.com>
2023-05-22 18:48:55 +0300

    Working epsilon query

--- examples/toy_client_inference_maat.py
+++ examples/toy_client_inference_maat.py
@@ -4,13 +4,21 @@ from pise import sym_ex_maat, server, hooks, sym_ex_helpers_maat
 import maat, time
 
 START_ADDRESS = 0x0
-BINARY_PATH = '/Users/evyatarziv/Documents/Technion/Spring 2023/ISC236349/PISE-MAAT/examples/toy_example'
+BINARY_PATH = './examples/toy_example'
+
+MAIN_OFFSET = 0x1309
+SOCKET_OFFSET = 0x1214
+CONNECT_OFFSET = 0x1204
+SCANF_OFFSET = 0x11e4
+RECV_OFFSET = 0x1134
+SEND_OFFSET = 0x1174
+
 
 class ToySendHook(hooks.CallSite):
 
     def set_hook(self, engine: maat.MaatEngine, pise_attr: sym_ex_helpers_maat.PISEAttributes):
         send_hook = hooks.SendHook(self)
-        engine.hooks.add(maat.EVENT.EXEC, maat.WHEN.BEFORE, filter=0x1174 + START_ADDRESS,
+        engine.hooks.add(maat.EVENT.EXEC, maat.WHEN.BEFORE, filter=SEND_OFFSET+sym_ex_maat.BASE_ADDR,
                          callbacks=[send_hook.make_callback(pise_attr)])
 
     def extract_arguments(self, call_context):
@@ -23,7 +31,7 @@ class ToyRecvHook(hooks.CallSite):
 
     def set_hook(self, engine: maat.MaatEngine, pise_attr: sym_ex_helpers_maat.PISEAttributes):
         recv_hook = hooks.RecvHook(self)
-        engine.hooks.add(maat.EVENT.EXEC, maat.WHEN.BEFORE, filter=0x1134 + START_ADDRESS,
+        engine.hooks.add(maat.EVENT.EXEC, maat.WHEN.BEFORE, filter=RECV_OFFSET+sym_ex_maat.BASE_ADDR,
                          callbacks=[recv_hook.make_callback(pise_attr)])
 
     def extract_arguments(self, call_context):
@@ -47,11 +55,10 @@ def main():
     logging.getLogger('pise').setLevel(logging.DEBUG)
     # logging.getLogger('angr').setLevel(logging.INFO)
     query_runner = sym_ex_maat.QueryRunner(BINARY_PATH,
-                                           [ToySendHook(), ToyRecvHook(), hooks.SocketHook(0x1214),
-                                            hooks.ConnectHook(0x1204)])
-    """,
-                                            hooks.HtonsHook(0x1164), hooks.InetPtonHook(0x11d4),
-                                            hooks.ConnectHook(0x1204), ToyScanfHook(0x11e4)])"""
+                                           [ToySendHook(),
+                                            ToyRecvHook(),
+                                            hooks.SocketHook(SOCKET_OFFSET),
+                                            hooks.ConnectHook(CONNECT_OFFSET)], MAIN_OFFSET)
     s = server.Server(query_runner)
     s.listen()
 
