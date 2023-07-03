33777b04c4dfa52bc53989df5efa3fc4bf482fe8
EvyatarZiv <evyatarziv@gmail.com>
2023-07-03 18:00:43 +0300

    Probing fixes

--- pise/hooks.py
+++ pise/hooks.py
@@ -99,11 +99,11 @@ class StrcmpHook(LibcCallSite):
             pise_attr.add_constraint(engine.cpu.rax == 0)
             idx = 0
             while True:
-                ch = engine.mem.read(s2_ptr.as_uint(engine.vars), 1)
+                ch = engine.mem.read(s2_ptr.as_uint(engine.vars)+idx, 1)
                 pise_attr.add_constraint(engine.mem.read(s1_ptr.as_uint(engine.vars)+idx, 1) == ch)
                 if ch.as_uint(engine.vars) == 0x0:
                     break
-                print(idx)
+                print(idx, ch.as_uint(engine.vars))
                 idx += 1
             if not pise_attr.gen_solver().check():
                 pise_attr.pop_engine_state(engine)
