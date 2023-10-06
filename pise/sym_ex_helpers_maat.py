53563be5b5ecc4ebb05cf22def6d5dbb8c07857a
EvyatarZiv <evyatarziv@gmail.com>
2023-10-06 22:28:10 +0300

    Logging fixed

--- pise/sym_ex_helpers_maat.py
+++ pise/sym_ex_helpers_maat.py
@@ -81,6 +81,7 @@ class PISEAttributes:
         self._solvers = entry[2]
         self.idx = entry[3]
         engine.vars.update_from(self.make_model())
+        assert not self.state_manager.dequeue_state(engine)
         logging.debug(f'Set to cached state @ {engine.cpu.rip}')
         return engine
 
