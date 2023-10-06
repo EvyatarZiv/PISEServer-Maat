eb21c7f54a2bde447a9fde53ff709a05b66911ea
EvyatarZiv <evyatarziv@gmail.com>
2023-10-06 17:09:54 +0300

    Logging fixed

--- pise/sym_ex_maat.py
+++ pise/sym_ex_maat.py
@@ -17,7 +17,7 @@ class QueryRunner:
         self.pise_attr = None
         self.mode = None
         self.engine.load(self.file, maat.BIN.ELF64, libdirs=[LIB64_PATH], load_interp=True, base=BASE_ADDR)
-        # logger.debug(self.engine.mem)
+        logger.setLevel(logging.DEBUG)
         self.callsites_to_monitor = callsites_to_monitor
         self.addr_main = addr_main
 
