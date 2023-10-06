77d7685b5b957250bfa82a55c69f8a27629b605c
EvyatarZiv <evyatarziv@gmail.com>
2023-10-06 19:13:14 +0300

    Logging fixed

--- pise/sym_ex_maat.py
+++ pise/sym_ex_maat.py
@@ -102,7 +102,7 @@ class QueryRunner:
             self.engine = self.pise_attr.set_cached_state(best_pref, self.engine)
             logger.debug(f'Prefix {best_pref} found!')
             #logger.debug(f'{self.engine.cpu.rip}')
-            self.engine.settings.log_insts = True
+            # self.engine.settings.log_insts = True
         else:
             self.engine = sym_ex_helpers_maat.PISEAttributes.set_init_state(self.engine)
             logger.debug('No prefix found')
