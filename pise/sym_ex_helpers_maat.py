49726a6262263ea968f804e449146bc48a3f15ba
EvyatarZiv <evyatarziv@gmail.com>
2023-10-06 23:58:32 +0300

    Logging fixed

--- pise/sym_ex_helpers_maat.py
+++ pise/sym_ex_helpers_maat.py
@@ -63,7 +63,7 @@ class PISEAttributes:
         manager = maat.SimpleStateManager(path)
         manager.enqueue_state(engine)
         PISEAttributes.state_cache_map[tuple(state)] = (
-            manager, self.solver, [self._solvers[-1].copy()] if self._solvers != [] else [[]], self.idx)
+            manager, self.solver, self._solvers[-1].copy() if self._solvers != [] else [], self.idx)
         NCACHED += 1
 
     def get_best_cached_prefix(self, state):
@@ -81,7 +81,7 @@ class PISEAttributes:
         entry[0].dequeue_state(engine)
         entry[0].enqueue_state(engine)
         self.solver = entry[1]
-        self._solvers = entry[2]
+        self._solvers = [entry[2].copy()]
         assert self._solvers != [] and self._debug_nstates_enq == 0 and self.gen_solver().check()
         self.idx = entry[3]
         engine.vars.update_from(self.make_model())
